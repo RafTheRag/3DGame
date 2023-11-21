ecca1049c89dc68f5f86500dbd4fb0f4be5fa2df
RafTheRag <ramirezrafael293@icloud.com>
2023-11-21 15:47:55 -0800

    Initial Commits

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInput playerInput;
+    public PlayerInput.OnFootActions onFoot;
+    private PlayerMotor motor;
+    private PlayerLook look;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerInput = new PlayerInput();
+        onFoot = playerInput.OnFoot;
+        motor = GetComponent<PlayerMotor>();   
+        onFoot.Jump.performed += ctx => motor.Jump();     
+        look = GetComponent<PlayerLook>();
+        onFoot.Crouch.performed += ctx => motor.Crouch();
+        onFoot.Sprint.performed += ctx => motor.Sprint();
+    }
+
+    void Update(){
+        motor.ProcessMove(onFoot.Movement.ReadValue<Vector2>());    
+        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());   
+    }
+
+    // Update is called once per frame
+
+    private void OnEnable(){
+        onFoot.Enable();
+    }
+
+    private void OnDisable(){
+        onFoot.Disable();
+    }
+}
