1c050115bd9d3660c8f3b7c70169b25612af23a2
RafTheRag <ramirezrafael293@icloud.com>
2023-12-12 00:43:23 -0800

    All AI Scripts Are In

---
+++ Assets/Enemy/Bullet.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision){
+        Transform hitTransform = collision.transform;
+        if(hitTransform.CompareTag("Player")){
+            hitTransform.GetComponent<PlayerHealth>().TakeDamage(10);
+        }
+        Destroy(gameObject);
+    }
+}
--- Assets/Enemy/Enemy.cs
+++ Assets/Enemy/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -10,17 +11,49 @@ public class Enemy : MonoBehaviour
     public NavMeshAgent Agent { get => agent; }
     [SerializeField] private string currentState;
     public AIPath path;
+    private GameObject player;
+    public float sightDistance = 20f;
+    public float fieldOfView = 85f;
+    public float eyeHeight;
+
+    public Transform gunBarrel;
+    [Range(0.1f, 10f)]
+    public float firerate;
+    public GameObject Player { get => player; }
+    private Vector3 lastKnownPosition;
+    public Vector3 LastKnownPosition {get => lastKnownPosition; set => lastKnownPosition = value; }
     // Start is called before the first frame update
     void Start()
     {
         stateMachine = GetComponent<StateMachine>();
         agent = GetComponent<NavMeshAgent>();
         stateMachine.Initialize();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CanSeePlayer();
+        currentState = stateMachine.activeState.ToString();
+    }
+
+    public bool CanSeePlayer(){
+        if(player != null){
+            if(Vector3.Distance(transform.position, player.transform.position) < sightDistance){
+                Vector3 targetDirection = player.transform.position - transform.position - (Vector3.up * eyeHeight);
+                float angleToPlayer = Vector3.Angle(targetDirection, transform.forward);
+                if(angleToPlayer >= -fieldOfView && angleToPlayer <= fieldOfView){
+                    Ray ray = new Ray(transform.position + (Vector3.up * eyeHeight), targetDirection);
+                    RaycastHit hitInfo = new RaycastHit();
+                    if(Physics.Raycast(ray, out hitInfo, sightDistance)){
+                        if(hitInfo.transform.gameObject == player){
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
